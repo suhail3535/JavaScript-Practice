f57a2d16e2047a9a8012e0b54c45ab330b33b4be
suhail khan <khansuhail9598@gmail.com>
2024-01-28 16:57:39 +0530

    css grid practice

---
+++ Batch2/grid/index.js
@@ -0,0 +1,8 @@
+// let arr = [0,1, 2, 3, 4, 7,5, 6];
+let a = 10;
+let b = 2; let c = 23;
+let minValue = Math.min(a,b,c);
+let maxValue = Math.max(a,b,c);
+
+console.log("Minimum value:", minValue);
+console.log("Maximum value:", maxValue);
