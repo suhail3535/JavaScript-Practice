724e2db02505872eed16bdf6e7cb158e2fd8e2b7
suhail khan <khansohail015@gmail.com>
2024-05-23 11:29:28 +0530

    basic interview concept practice

--- FrontendInterview(HTML,CSS,JavaScriptREACT)/index.js
+++ FrontendInterview(HTML,CSS,JavaScriptREACT)/index.js
@@ -1,1 +1,23 @@
 
+let data = [
+    {name:"suhail khan",mobile:998888888},
+    {name:"sohan",mobile:998888888},
+    {name:"aman",mobile:998888888}
+]
+
+
+let data2 = [
+    { name: "suhail khan", mobile: 998888888 },
+    { name: "sohan", mobile: 998888888 },
+    { name: "aman", mobile: 998888888 }
+]
+function value (details,details2) {
+    details.map((ele) => {
+        console.log(ele);
+    })
+    details2.map((ele) => {
+        console.log(ele);
+    })
+}
+value(data,data2)
+// value(data2)
