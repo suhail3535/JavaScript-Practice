3244a6865a5aff7f725bcd83bbd34a7041cca51d
suhail khan <khansohail015@gmail.com>
2024-05-24 08:46:46 +0530

    crud operation practice react

--- FrontendInterview(HTML,CSS,JavaScriptREACT)/myapp/src/App.js
+++ FrontendInterview(HTML,CSS,JavaScriptREACT)/myapp/src/App.js
@@ -1,10 +1,12 @@
 
 import Card from './pages/Card';
+import Contact from './pages/Contact';
+// import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Contact />
     </div>
   );
 }
